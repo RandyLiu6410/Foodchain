783f18805f43f3cbd9eb4e4ec0790ebf4375b362
TsouTony <51122236+TsouTony@users.noreply.github.com>
2021-01-10 02:53:52 +0800

    Add files via upload

--- server/model/food.model.js
+++ server/model/food.model.js
@@ -4,15 +4,24 @@ const Schema = mongoose.Schema;
 
 const foodSchema = new Schema({
     logno: {type: Number, require: true},
+    content: [{
+      logname: {type: String, require: true},
+      logorg: {type: String, require: true},
+    }],
+    image: [{
+      url: {type: String, require: true},
+      filehash : {type: String, require: true},
+    }],
+    item: [{
+      loghash: {type: String, require: true},
+      logdate: {type: String, require: true},
+    }],
     section: [{
+      // transactionhash: {type: String, require: true},
       title: {type: String, require: true},
       begin: {type: String, require: true},
       end: {type: String, require: true},
-      url: {type: String, require: true}
     }],
-    logname: {type: String, require: true},
-    logorg: {type: String, require: true},
-    logdate: {type: String, require: true},
     },
     { timestamps: true }
   );
